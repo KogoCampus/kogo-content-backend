eb33bb42ac03e39790ba83afff51213cbd22a811
scott0929 <scott0929@gmail.com>
2024-10-22 00:53:43 -0700

    fix: tests

--- src/test/kotlin/com/kogo/content/service/CommentServiceTest.kt
+++ src/test/kotlin/com/kogo/content/service/CommentServiceTest.kt
@@ -4,17 +4,21 @@ import com.kogo.content.endpoint.model.CommentDto
 import com.kogo.content.endpoint.model.CommentUpdate
 import com.kogo.content.storage.entity.Comment
 import com.kogo.content.storage.entity.CommentParentType
+import com.kogo.content.storage.entity.Post
 import com.kogo.content.storage.entity.UserDetails
 import com.kogo.content.storage.repository.CommentRepository
 import com.kogo.content.storage.repository.LikeRepository
+import com.kogo.content.storage.repository.PostRepository
 import io.mockk.*
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import org.springframework.data.repository.findByIdOrNull
 
 class CommentServiceTest {
     private val commentRepository: CommentRepository = mockk()
+    private val postRepository: PostRepository = mockk()
     private val likeRepository: LikeRepository = mockk()
-    private val commentService: CommentService = CommentService(commentRepository, likeRepository,)
+    private val commentService: CommentService = CommentService(commentRepository, postRepository, likeRepository)
 
     @Test
     fun `should find comments by parentId`() {
@@ -26,6 +30,7 @@ class CommentServiceTest {
             content = "comment-content",
             parentType = CommentParentType.POST,
             owner = user,
+            replies = emptyList()
         )
 
         every { commentRepository.findAllByParentId(parentId) } returns listOf(result)
@@ -46,7 +51,15 @@ class CommentServiceTest {
         val commentDto = mockk<CommentDto>() {
             every { content } returns "comment content"
         }
+        val post = mockk<Post>(relaxed = true) {
+            every { comments } returns emptyList() // Mock comments list
+        }
+
+        every { postRepository.findByIdOrNull(postId) } returns post
         every { commentRepository.save(any()) } returns mockk()
+        every { postRepository.save(post) } returns post
+
+        // Act
         commentService.create(postId, CommentParentType.POST, user, commentDto)
 
         verify {
@@ -55,8 +68,13 @@ class CommentServiceTest {
                 assertThat(it.owner).isEqualTo(user)
                 assertThat(it.parentId).isEqualTo("post-id")
                 assertThat(it.parentType).isEqualTo(CommentParentType.POST)
+                assertThat(it.replies).isEmpty()
             })
         }
+        verify {
+            postRepository.save(post)
+        }
+
     }
 
     @Test
@@ -68,6 +86,7 @@ class CommentServiceTest {
             parentType = CommentParentType.POST,
             content = "comment-content",
             owner = user,
+            replies = emptyList()
         )
 
         val newComment = CommentUpdate(
@@ -81,6 +100,46 @@ class CommentServiceTest {
             commentRepository.save(withArg {
                 assertThat(it.content).isEqualTo(newComment.content)
             })
+        }
+    }
+
+    @Test
+    fun `should delete a comment`() {
+        val user = mockk<UserDetails>()
+        val comment = Comment(
+            id = "comment-id",
+            parentId = "parent-id",
+            parentType = CommentParentType.POST,
+            content = "comment-content",
+            owner = user,
+            replies = listOf("reply-id-1", "reply-id-2")
+        )
+        val post = mockk<Post>(relaxed = true) {
+            every { comments } returns listOf(comment)
+        }
+        val replies = listOf(
+            Comment(id = "reply-id-1", parentId = comment.id!!, parentType = CommentParentType.COMMENT, replies = emptyList(), content = "comment-content", owner = user,),
+            Comment(id = "reply-id-2", parentId = comment.id!!, parentType = CommentParentType.COMMENT, replies = emptyList(), content = "comment-content", owner = user,)
+        )
+
+        // Mock repository calls
+        every { postRepository.findByIdOrNull(comment.parentId) } returns post
+        every { commentRepository.findByIdOrNull(comment.id!!) } returns comment
+        every { commentRepository.findAllById(comment.replies) } returns replies
+        every { commentRepository.deleteById(any()) } just Runs
+
+        every { postRepository.save(post) } returns post
+        every { commentRepository.save(any()) } returns comment
+
+        // Act
+        commentService.delete(comment)
+
+        verify {
+            commentRepository.deleteById(comment.id!!)
+            replies.forEach { reply ->
+                commentRepository.deleteById(reply.id!!)
+            }
+            postRepository.save(post)
         }
     }
 
