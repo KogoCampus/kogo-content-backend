191ecd37e7dabbf4e59f50f7fe4cf339ea4e0c4d
Jiin Kim <jiinkim@Jiins-MacBook-Pro.local>
2024-10-22 20:39:37 -0400

    fix: allow cors

--- src/main/kotlin/com/kogo/content/endpoint/WebMvcConfiguration.kt
+++ src/main/kotlin/com/kogo/content/endpoint/WebMvcConfiguration.kt
@@ -1,14 +1,28 @@
 package com.kogo.content.endpoint
 
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.servlet.config.annotation.CorsRegistry
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
 
 @Configuration
 class WebMvcConfiguration : WebMvcConfigurer {
     override fun addInterceptors(registry: InterceptorRegistry) {
         registry.addInterceptor(RequestInterceptor()).addPathPatterns("/media/**")
         super.addInterceptors(registry)
+    }
+
+    @Bean
+    fun addCorsConfig(): WebMvcConfigurer {
+        return object : WebMvcConfigurer {
+            override fun addCorsMappings(registry: CorsRegistry) {
+                registry.addMapping("/**")
+                    .allowedMethods("*")
+                    .allowedOrigins("*")
+                    .allowCredentials(false)
+            }
+        }
     }
 }
