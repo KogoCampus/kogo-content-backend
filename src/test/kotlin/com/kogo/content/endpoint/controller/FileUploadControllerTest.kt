ab00e613db854ab8e188a3d0a938bc2571117b19
Jiin Kim <jiinkim@Jiins-MacBook-Pro.local>
2025-02-05 09:05:45 -0500

    fix: test failure

--- src/test/kotlin/com/kogo/content/endpoint/controller/FileUploadControllerTest.kt
+++ src/test/kotlin/com/kogo/content/endpoint/controller/FileUploadControllerTest.kt
@@ -1,25 +1,23 @@
 package com.kogo.content.endpoint.controller
 
+import com.kogo.content.service.UserService
 import com.kogo.content.service.fileuploader.FileUploaderService
 import com.kogo.content.storage.model.Attachment
 import com.ninjasquad.springmockk.MockkBean
 import io.mockk.every
-import io.mockk.verify
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.test.autoconfigure.web. servlet.AutoConfigureMockMvc
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.mock.web.MockMultipartFile
-import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.web.servlet.*
 
 @SpringBootTest
 @AutoConfigureMockMvc(addFilters = false)
-@ActiveProfiles("test")
 class FileUploadControllerTest @Autowired constructor(
     private val mockMvc: MockMvc
 )  {
+    @MockkBean private lateinit var userService: UserService
     @MockkBean private lateinit var fileUploaderService: FileUploaderService
 
     private lateinit var testAttachment: Attachment
